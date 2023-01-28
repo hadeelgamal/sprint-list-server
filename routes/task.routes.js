be1ce16d49152f4271a79539f6bf4c1a51789726
Hadeel Abdelkareem <43421729+hadeelgamal@users.noreply.github.com>
2023-01-28 17:02:38 +0200

    updated task routes

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -1,31 +1,45 @@
 const router = require("express").Router();
-const mongoose = require('mongoose');
-
-const Task = require("../models/Task.model")
+const mongoose = require("mongoose");
+const { findById } = require("../models/Sprint.model");
+const Sprint = require("../models/Sprint.model");
+const Task = require("../models/Task.model");
 
 //  POST /api/tasks  -  Creates a new task
-router.post('/tasks', (req, res, next) => {
-    const { description, dueDate, checked, sprintId } = req.body;
-   
-    Task.create({ description, dueDate, checked, sprint: sprintId })
-      .then(response => res.json(response))
-      .catch(err => res.json(err));
+router.post("/tasks", async (req, res, next) => {
+  const { description, dueDate, checked, sprintId } = req.body;
+
+  const newTask = await Task.create({
+    description,
+    dueDate,
+    checked,
+    sprint: sprintId,
   });
 
- 
-// DELETE - /api/tasks/:taskId  - Delete specified project
+  Sprint.findByIdAndUpdate(sprintId, { $push: { tasks: newTask._id } })
+    .then((foundSprint) => {
+      return foundSprint.save();
+    })
+    .then(() => {
+      res.json(newTask);
+    })
+    .catch((err) => res.json(err));
+});
 
-router.delete("/tasks/:taskId ", (req, res)=>{
-    const { taskId } = req.params;
-    
-    if (!mongoose.Types.ObjectId.isValid(taskId)) {
-        res.status(400).json({ message: 'Specified id is not valid' });
-        return;
-      }
+// DELETE - /api/tasks/:taskId  - Delete specified task
 
-    Task.findByIdAndDelete(taskId)
-            .then(deletedTask => res.json(deletedTask))
-            .catch(err => console.log(err))
-})
+router.delete("/tasks/:sprintId/:taskId", async (req, res) => {
+  const { taskId, sprintId } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(taskId)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  const deletedTask = await Task.findByIdAndDelete(taskId);
+  Sprint.findByIdAndUpdate(sprintId, { $pull: {tasks: deletedTask._id } })
+  .then(updatedSprint => {
+    updatedSprint.save()
+    res.json(updatedSprint.tasks)})
+    .catch((err) => console.log(err));
+});
 
 module.exports = router;
